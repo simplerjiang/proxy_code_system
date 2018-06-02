b8f1821715cd8c1db8bc069b19323700cff6565b
Kong <jiangsimpler@gmail.com>
2018-06-02 21:10:45 +0800

    Fix Migrations file

--- main_app/migrations/0026_auto_20180530_1255.py
+++ main_app/migrations/0026_auto_20180530_1255.py
@@ -11,14 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Software_cost',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('software_each_time', models.PositiveIntegerField(verbose_name='套餐时间（按小时计算）', default=720)),
-                ('software_cost', models.DecimalField(verbose_name='软件价格', default=0, max_digits=8, decimal_places=2)),
-            ],
-        ),
         migrations.RemoveField(
             model_name='software',
             name='software_cost',
@@ -26,10 +18,5 @@ class Migration(migrations.Migration):
         migrations.RemoveField(
             model_name='software',
             name='software_each_time',
-        ),
-        migrations.AddField(
-            model_name='software_cost',
-            name='software',
-            field=models.ForeignKey(verbose_name='软件对象', to='main_app.Software'),
         ),
     ]
